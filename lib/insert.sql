ec19499a6000fa8d0ccb9f5e78712d979a770704
Tracy Reuther <tracyreuther@gmail.com>
2019-08-09 13:50:19 +0000

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,5 +1,12 @@
-INSERT INTO users (name, age)
-  VALUES ("Bob", 20);
-
-INSERT INTO users (name, age)
-  VALUES ("Tom", 21);
+INSERT INTO users (name, age) VALUES
+("Bob", 20),
+("Tom", 21),
+("Ann", 22),
+("Carol", 23),
+("Troy", 24),
+("Preston", 25),
+("Tracy", 26),
+("DonL", 27),
+("Erica", 28),
+("Danielle", 29),
+("Timmy", 30);
