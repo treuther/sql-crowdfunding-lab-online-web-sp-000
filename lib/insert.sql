cf95be684e6d0c4652d6a14d4efa533af7fceb6f
Tracy Reuther <tracyreuther@gmail.com>
2019-08-09 14:03:41 +0000

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -27,4 +27,6 @@ INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUE
   ("new floors", "remodel", 3000, "4/1/2019", "9/30/2019"),
   ("new wardrobe", "work", 2000, "5/1/2019", "10/31/2019"),
   ("office supplies", "work", 100, "6/1/2019", "11/30/2019"),
+  ("xmas gifts", "holiday", 2500, "7/1/2019", "12/20/2019"),
+  
   
